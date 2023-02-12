b50acb74d2d0e017d8cdecbce997b3189183cac7
drew <shanetydrew-byte@proton.me>
2023-02-12 16:47:51 +0800

    i need logic in playBtn

--- Music/src/mainpackage/Main.java
+++ Music/src/mainpackage/Main.java
@@ -1,6 +1,7 @@
 package mainpackage;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,6 +10,7 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.SystemColor;
 import java.awt.event.ActionListener;
@@ -16,6 +18,8 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -26,7 +30,7 @@ public class Main {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws UnsupportedAudioFileException, IOException, LineUnavailableException{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -45,6 +49,12 @@ public class Main {
 	public Main() throws UnsupportedAudioFileException, IOException, LineUnavailableException  {
 		initialize();
 	}
+	
+	boolean playing = true;
+	
+	AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\Shane\\Documents\\Github\\java_code\\JavaAlgo\\music\\twice_wil.wav").getAbsoluteFile());
+	Clip clip = AudioSystem.getClip();
+	
 
 	/**
 	 * Initialize the contents of the frame.
@@ -64,6 +74,7 @@ public class Main {
 		JButton select = new JButton("Select");
 		select.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showConfirmDialog(null,"Select a data here");
 			}
 		});
 		select.setBounds(10, 11, 364, 47);
@@ -79,62 +90,64 @@ public class Main {
 		lblNewLabel.setBounds(139, 28, 85, 14);
 		panel_1.add(lblNewLabel);
 		
-		JButton back = new JButton("Back");
+		JButton back = new JButton("");
 		back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		back.setBounds(10, 154, 89, 47);
+		back.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\Shane\\Desktop\\Music Project\\fast-backward.png").getImage()
+				.getScaledInstance(40, 40, Image.SCALE_DEFAULT)));
+		back.setBounds(10, 154, 79, 47);
 		panel.add(back);
 		
 		
 		
-		JButton playpause = new JButton("Play/Pause");
-		playpause.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int count = 0;
-				boolean playing = true;
+		JButton play = new JButton("");
+		play.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				try {
+					
+					clip.open(audioInputStream);
+					long clipTimePos = 0;
+					
+					if(e.getClickCount()%2==1 && playing)
+					{
+						clip.start();
+						playing = false;
+					}
+					else
+					{
+						clip.stop();
+						playing = true;
+						
+					}
 				
-					try {
-						Clip clip = AudioSystem.getClip();
-						
-						count++;
-						if(count%2==1 && playing)
-						{
-							AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\Shane\\Documents\\Github\\java_code\\JavaAlgo\\music\\twice_wil.wav").getAbsoluteFile());
-							
-							clip.open(audioInputStream);
-							clip.start();
-							playing = false;
-						}
-						else if(playing==false)
-						{
-							clip.stop();
-							playing = true;
-						}
-						
-						
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} 
-					
-					
-				}
-			
-		});
-		
-
-		
-
-		
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} 
 	
-		playpause.setBounds(148, 154, 89, 47);
-		panel.add(playpause);
+				
+			}
+		});
+		play.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\Shane\\Desktop\\Music Project\\play.png").getImage()
+				.getScaledInstance(40, 40, Image.SCALE_DEFAULT)));
+	
+		play.setBounds(149, 154, 79, 47);
+		panel.add(play);
 		
-		JButton next = new JButton("Next");
-		next.setBounds(285, 154, 89, 47);
+		JButton next = new JButton("");
+		next.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		next.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\Shane\\Desktop\\Music Project\\fast-forward.png").getImage()
+				.getScaledInstance(40, 40, Image.SCALE_DEFAULT)));
+		next.setBounds(295, 154, 79, 47);
 		panel.add(next);
+		
 		
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
